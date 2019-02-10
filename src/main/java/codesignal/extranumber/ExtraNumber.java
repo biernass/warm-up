c81b9cdf0caf8eb881c41f4eaf3b177efd11f101
Michał Biernacki <micbiernacki@gmail.com>
2019-02-10 20:25:32 +0100

    test preparing

---
+++ src/main/java/codesignal/extranumber/ExtraNumber.java
@@ -0,0 +1,8 @@
+package codesignal.extranumber;
+
+public class ExtraNumber {
+
+    public int extraNumber(int a, int b, int c){
+        return 0;
+    }
+}
---
+++ src/test/java/codesignal/extranumber/ExtraNumberTest.java
@@ -0,0 +1,44 @@
+package codesignal.extranumber;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExtraNumberTest {
+
+
+    private ExtraNumber number;
+
+    @Before
+    public void setup(){
+        number = new ExtraNumber();
+    }
+
+    @Test
+    public void shouldReturnSeven(){
+        int result = number.extraNumber(2, 7, 2);
+        assertEquals(7, result);
+    }
+
+    @Test
+    public void shouldReturnThree(){
+        int result = number.extraNumber(3, 2, 2);
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void shouldReturnOne(){
+        int result = number.extraNumber(5, 5, 1);
+        assertEquals(1, result);
+    }
+
+
+    @Test
+    public void shouldReturnThree2(){
+        int result = number.extraNumber(500000000, 3, 500000000);
+        assertEquals(1, result);
+    }
+
+
+}
