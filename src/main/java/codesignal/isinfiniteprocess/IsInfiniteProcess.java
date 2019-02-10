e081d2e01f2bb9ab39e558638859af2ca513ca57
Michał Biernacki <micbiernacki@gmail.com>
2019-02-10 20:59:16 +0100

    solution IsInfiniteProcess

--- src/main/java/codesignal/isinfiniteprocess/IsInfiniteProcess.java
+++ src/main/java/codesignal/isinfiniteprocess/IsInfiniteProcess.java
@@ -2,7 +2,7 @@ package codesignal.isinfiniteprocess;
 
 public class IsInfiniteProcess {
 
-    public boolean isInfiniteProcess(int a, int b){
-
+    public boolean isInfiniteProcess(int a, int b) {
+        return a > b || a % 2 != b % 2;
     }
 }
